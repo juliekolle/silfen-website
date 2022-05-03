697b38d85df1ece92090901599bb31e272184014
josefinabertuccioli <jose500k@stud.kea.dk>
2022-05-03 15:21:59 +0200

    template img and name

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+//const url = "https://jomediart.dk/wordpress/wp-json";
+window.addEventListener("load", setup);
+//const endpoint = "https://jomediart.dk/wordpress/wp-json/wp/v2/product_list";
 
-const url = "https://jomediart.dk/wordpress/wp-json";
+function setup(){
+    const template = document.querySelector("bag_templete")
+    getProduct_list();
+}
+function getProduct_list(){
+    fetch("https://jomediart.dk/wordpress/wp-json/wp/v2/product_list?_embed")
+    .then(res => res.json())
+    .then(setupProduct_list);
+}
 
-fetch (url)
- .then(function(res){
-     return res.json();
- })
+function setupProduct_list(bags){
+    //console.log(bags)
+    bags.forEach(showbag)  
+}
 
- .then(function(data){
- handleProductList(data);
+function showbag(bag){
+    const template = document.querySelector("template").content;
+    const clone= template.cloneNode(true);
+    console.log(bag);
+    clone.querySelector("h3").textContent = bag.title.rendered;
+    const imgurl = bag._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    clone.querySelector("img").src = imgurl
 
- })
+  
 
- function handleProductList(data){
+   //console.log(price);
+//const currency = price._embedded
+//clone.querySelector(".price").textContent = price;
+document.querySelector("main").appendChild(clone);
 
-data.forEach(showProduct);
- }
+
+}
+
+//*function showprice(price){
+    //const template = document.querySelector("template").content;
+    //const clone = template.cloneNode(true);
+    //console.log(price);
+   // clone.querySelector("p").textContent = price;
+
+//}
